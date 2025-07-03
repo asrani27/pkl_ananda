156f19b107922b6141d15c638c6779bd969aec9a
Your Name <you@example.com>
2025-07-03 18:59:17 +0800

    f

--- resources/views/admin/pegawai/detail.blade.php
+++ resources/views/admin/pegawai/detail.blade.php
@@ -49,7 +49,6 @@
                                     <h3>{{$data->jabatan->nama_jabatan}}</h3>
                                     <h3>{{$data->bagian == null ? '': $data->bagian->nama_bagian}}</h3>
                                     <h3>{{$data->golongan == null ? '': $data->golongan->nama_golongan}}</h3>
-                                    {{-- <h3><strong>Golongan {{$data->golongan}}</h3> --}}
                                     <p><strong>NIP : </strong>{{$data->nip}}</p>
                                     <p><strong>NIK : </strong>{{$data->nik}}</p>
                                     <p><strong>TTL : </strong>{{$data->ttl}}</p>
