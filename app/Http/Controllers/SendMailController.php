1bb97ebe071c3de023b0ef120ecaa5a3873a3cc7
Your Name <you@example.com>
2025-09-03 16:57:24 +0800

    f

--- app/Http/Controllers/SendMailController.php
+++ app/Http/Controllers/SendMailController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Mail;
 use App\Mail\SendMail;
 use App\Models\Upload;
+use App\Models\Pegawai;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 
@@ -12,17 +13,22 @@ class SendMailController extends Controller
 {
     public function index($id)
     {
-        $data = Upload::find($id)->user->pegawai;
-
+        $data = Pegawai::find($id);
+        
         return view('admin.dokumen.sendmail', compact('data'));
     }
 
     public function kirim(Request $req, $id)
     {
         $details = $req->all();
+        if($req->email == null){    
+        Session::flash('error', 'harap isi email');
+        return redirect('admin/data/dokumen');
+        }else{
 
         \Mail::to($req->email)->send(new SendMail($details));
         Session::flash('success', 'email berhasil di kirim');
         return redirect('admin/data/dokumen');
+        }
     }
 }
